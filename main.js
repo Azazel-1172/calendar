feaaf2ff5cba7deba11b912d7346becafc1d9b70
Azazel-1172 <gordon70705@gmail.com>
2023-05-21 23:22:40 +0800

    add SweetAlert & repair day calendar style

--- main.js
+++ main.js
@@ -1,4 +1,3 @@
-// let SHEET_ID = "1dr9W9R3zbh-UcnwjaeXxmwX5I2FD_ag1_jBK3bIIqDk"; 測試正常版
 let SHEET_ID = "1kgPFr1xCvP_QnKoeDCh2cYmyRWNHNxNS4YGnSfUyjkc";
 let SHEET_TITLE = "表單回應 1";
 let SHEET_RANGE = [];
@@ -21,100 +20,189 @@ async function fetchData(FULL_URL) {
   }
 }
 
-fetchData(FULL_URL).then((data) => {
-  console.log(data.table.rows[0].c[1].v);
-});
-
-fetchData(FULL_URL).then((data) => {
-  console.log(data.table.rows[0].c[4].f);
-});
-
-// fetch(FULL_URL)
-//   .then((res) => res.text())
-//   .then((rep) => {
-//     let data = JSON.parse(rep.substr(47).slice(0, -2));
-//   });
-
 document.addEventListener("DOMContentLoaded", function () {
   var calendarEl = document.getElementById("calendar");
-  var calendar = new FullCalendar.Calendar(calendarEl, {
-    initialView: "dayGridMonth",
-    locale: "zh-tw",
-    headerToolbar: {
-      left: "prev,today,next",
-      center: "title",
-      right: "dayGridWeek,dayGridDay,dayGridMonth", // user can switch between the two
-    },
 
-    events: [
-      {
-        title: fetchData(FULL_URL).then((data) => {
-          data.table.rows[0].c[1].v;
-        }),
-        start: fetchData(FULL_URL).then((data) => {
-          console.log(data.table.rows[0].c[4].v);
-        }),
-      },
-      {
-        title: "測試",
-        start: "2023-05-21T14:30:00",
-        end: "2023-05-21T18:30:00",
-      },
-      {
-        title: "吃飯",
-        start: "2023-05-22T14:30:00",
-        end: "2023-05-22T18:30:00",
-      },
-    ],
+  fetchData(FULL_URL).then((data) => {
+    var events = [];
+    var startTimeString, endTimeString;
+    var startTime = [];
+    var month;
+    var date;
+    var endTime = [];
 
-    // events: [
-    //   {
-    //     title: "測試",
-    //     start: "2023-05-21T14:30:00",
-    //     end: "2023-05-21T18:30:00",
-    //   },
-    //   {
-    //     title: "測試2",
-    //     start: "2023-05-21",
-    //     end: "2023-05-21",
-    //     allday: true,
-    //   },
-    //   {
-    //     title: "Meeting",
-    //     start: "2023-05-12T10:30:00",
-    //     end: "2023-05-12T12:30:00",
-    //   },
-    // ],
+    for (let i = 0; i < data.table.rows.length; i++) {
+      if (data.table.rows[i].c[5].f[0] == "下") {
+        start_hour = data.table.rows[i].c[5].v.slice(16, 18);
+        if (Number(data.table.rows[i].c[5].f.slice(3, 5))) {
+          startTimeString =
+            start_hour +
+            ":" +
+            data.table.rows[i].c[5].f.slice(6, 8) +
+            ":" +
+            data.table.rows[i].c[5].f.slice(9, 11);
+          // console.log(startTimeString);
+        } else {
+          startTimeString =
+            start_hour +
+            ":" +
+            data.table.rows[i].c[5].f.slice(5, 7) +
+            ":" +
+            data.table.rows[i].c[5].f.slice(8, 10);
+          // console.log(startTimeString);
+        }
+      } else {
+        if (Number(data.table.rows[i].c[5].f.slice(3, 5))) {
+          // 超過10點
+          start_hour = data.table.rows[i].c[5].v.slice(16, 18);
+          startTimeString =
+            start_hour +
+            ":" +
+            data.table.rows[i].c[5].f.slice(6, 8) +
+            ":" +
+            data.table.rows[i].c[5].f.slice(9, 11);
+          // console.log(startTimeString);
+        } else {
+          start_hour = data.table.rows[i].c[5].v.slice(16, 17);
+          startTimeString =
+            "0" +
+            start_hour +
+            ":" +
+            data.table.rows[i].c[5].f.slice(5, 7) +
+            ":" +
+            data.table.rows[i].c[5].f.slice(8, 10);
+          // console.log(startTimeString);
+        }
+      }
 
-    // eventClick: function (info) {
-    //   if (info.event.end) {
-    //     alert(
-    //       "Event: " +
-    //         info.event.title +
-    //         "\n" +
-    //         "Start Time: " +
-    //         info.event.start +
-    //         "\n" +
-    //         "End Time: " +
-    //         info.event.end +
-    //         "\n"
-    //     );
-    //   } else {
-    //     alert(
-    //       "Event: " +
-    //         info.event.title +
-    //         "\n" +
-    //         "Start Time: " +
-    //         info.event.start +
-    //         "\n" +
-    //         "End Time: " +
-    //         info.event.start +
-    //         "\n"
-    //     );
-    //   }
-    //   console.log(events);
-    // },
+      if (data.table.rows[i].c[8].f[0] == "下") {
+        end_hour = data.table.rows[i].c[8].v.slice(16, 18);
+        if (Number(data.table.rows[i].c[8].f.slice(3, 5))) {
+          endTimeString =
+            end_hour +
+            ":" +
+            data.table.rows[i].c[8].f.slice(6, 8) +
+            ":" +
+            data.table.rows[i].c[8].f.slice(9, 11);
+          // console.log(endTimeString);
+        } else {
+          endTimeString =
+            end_hour +
+            ":" +
+            data.table.rows[i].c[8].f.slice(5, 7) +
+            ":" +
+            data.table.rows[i].c[8].f.slice(8, 10);
+          // console.log(endTimeString);
+        }
+      } else {
+        if (Number(data.table.rows[i].c[8].f.slice(3, 5))) {
+          // 超過10點
+          end_hour = data.table.rows[i].c[8].v.slice(16, 18);
+          endTimeString =
+            end_hour +
+            ":" +
+            data.table.rows[i].c[8].f.slice(6, 8) +
+            ":" +
+            data.table.rows[i].c[8].f.slice(9, 11);
+          // console.log(endTimeString);
+        } else {
+          end_hour = data.table.rows[i].c[8].v.slice(16, 17);
+          endTimeString =
+            "0" +
+            start_hour +
+            ":" +
+            data.table.rows[i].c[8].f.slice(5, 7) +
+            ":" +
+            data.table.rows[i].c[8].f.slice(8, 10);
+          // console.log(endTimeString);
+        }
+      }
+
+      month = Number(data.table.rows[i].c[4].f.split("/")[1]);
+      date = Number(data.table.rows[i].c[4].f.split("/")[2]);
+
+      if (month < 10) {
+        month = "0" + JSON.stringify(month);
+      }
+      if (date < 10) {
+        date = "0" + JSON.stringify(date);
+      }
+
+      startTime[i] =
+        data.table.rows[i].c[4].f.split("/")[0] +
+        "-" +
+        month +
+        "-" +
+        date +
+        "T" +
+        startTimeString;
+      // console.log(startTime);
+      endTime[i] =
+        data.table.rows[i].c[4].f.split("/")[0] +
+        "-" +
+        month +
+        "-" +
+        date +
+        "T" +
+        endTimeString;
+      // console.log(endTime);
+      events.push({
+        title: data.table.rows[i].c[1].v,
+        start: startTime[i],
+        end: endTime[i],
+      });
+    }
+    popover = false;
+    // console.log(events);
+    var calendar = new FullCalendar.Calendar(calendarEl, {
+      initialView: "dayGridMonth",
+      locale: "zh-tw",
+      headerToolbar: {
+        left: "prev,today,next",
+        center: "title",
+        right: "timeGridWeek,timeGridDay,dayGridMonth",
+      },
+      events: events,
+
+      eventClick: function (info) {
+        swal(
+          "活動名稱: " +
+            info.event.title +
+            "\n" +
+            "開始時間: " +
+            new Date(info.event.start).getFullYear() +
+            "年" +
+            (new Date(info.event.start).getMonth() + 1)
+              .toString()
+              .padStart(2, "0") +
+            "月" +
+            new Date(info.event.start).getDate().toString().padStart(2, "0") +
+            "日" +
+            new Date(info.event.start).getHours() +
+            "時" +
+            new Date(info.event.start).getMinutes() +
+            "分" +
+            "\n" +
+            "結束時間: " +
+            new Date(info.event.end).getFullYear() +
+            "年" +
+            (new Date(info.event.end).getMonth() + 1)
+              .toString()
+              .padStart(2, "0") +
+            "月" +
+            new Date(info.event.end).getDate().toString().padStart(2, "0") +
+            "日" +
+            new Date(info.event.end).getHours() +
+            "時" +
+            new Date(info.event.end).getMinutes() +
+            "分" +
+            "\n"
+        );
+
+        // console.log(title);
+      },
+    });
+
+    calendar.render();
   });
-  console.log(calendar.currentData.eventStore);
-  calendar.render();
 });
